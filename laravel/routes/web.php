2248081c10a331b468cbc8162fde4f0c3660c8e4
msgreentea <msgreentea8@gmail.com>
2022-01-20 21:36:26 +0900

    editing blades

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,7 +5,13 @@ use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\AuthenticatedSessionController;
 use App\Http\Controllers\RegisteredUserController;
 
-
+// 打刻ページ
 Route::get('/', [AttendanceController::class, 'stamp'])->name('stamp');
+
+// ログインページ
 Route::get('/login', [AuthenticatedSessionController::class, 'login'])->name('login');
+
+// 会員登録ページ
 Route::get('/register', [RegisteredUserController::class, 'register'])->name('register');
+
+//
