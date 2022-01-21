1342f4f19bc9a4afbaa9f72ccd199227e508695e
MsGreentea <msgreentea8@gmail.com>
2022-01-21 16:47:09 +0900

    mmm

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\RegisteredUserController;
 
 // 打刻ページ
 Route::get('/', [StampController::class, 'stamp'])->name('stamp');
+Route::post('/punchin', [StampController::class, 'punchin'])->name('punchin');
+Route::post('/punchout', [StampController::class, 'punchout'])->name('punchout');
+Route::post('/breakin', [StampController::class, 'breakin'])->name('breakin');
+Route::post('/breakout', [StampController::class, 'breakout'])->name('breakout');
 
 // 日付別勤怠ページ
 Route::get('/attendance', [AttendanceController::class, 'attendance'])->name('attendance');
