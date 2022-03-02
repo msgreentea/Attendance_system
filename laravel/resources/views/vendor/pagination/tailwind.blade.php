bbc454d1cf7f1c05e2932c7fcb05d786f4926468
MsGreentea <msgreentea8@gmail.com>
2022-03-02 15:57:04 +0900

    pagination

--- laravel/resources/views/vendor/pagination/tailwind.blade.php
+++ laravel/resources/views/vendor/pagination/tailwind.blade.php
@@ -1,14 +1,3 @@
-<style>
-/* .text-gray-500 {
-    color: #224ae0;
-    background:red;
-}
-.active:text-gray-500 {
-    background-color: #224ae0;
-    color:white;
-} */
-</style>
-
 @if ($paginator->hasPages())
     <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-between">
         <div class="flex justify-between flex-1 sm:hidden">
@@ -34,7 +23,7 @@
         </div>
 
         <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-            <div>
+            {{-- <div>
                 <p class="text-sm text-gray-700 leading-5">
                     {!! __('Showing') !!}
                     @if ($paginator->firstItem())
@@ -48,7 +37,7 @@
                     <span class="font-medium">{{ $paginator->total() }}</span>
                     {!! __('results') !!}
                 </p>
-            </div>
+            </div> --}}
 
             <div>
                 <span class="relative z-0 inline-flex shadow-sm rounded-md">
@@ -82,11 +71,20 @@
                         @if (is_array($element)) {{-- selected --}}
                             @foreach ($element as $page => $url)
                                 @if ($page == $paginator->currentPage())
+                                {{-- 
+                                ** changed ** 
+                                bg-white ( background-color : white) to bg-blue-500
+                                text-gray-500 (color: gray) to text-white
+                                --}}
                                     <span aria-current="page">
-                                        <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5">{{ $page }}</span>
+                                        <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-blue-600 border border-gray-300 cursor-default leading-5">{{ $page }}</span>
                                     </span>
                                 @else {{-- unselected --}}
-                                    <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
+                                {{-- 
+                                **changed **
+                                text-gray-500 (color: gray) to text-blue-600
+                                 --}}
+                                    <a href="{{ $url }}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-blue-600 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
                                         {{ $page }}
                                     </a>
                                 @endif
